5bc98c6efe68a01e2bbfc9c2be35c9660bae744b
Bilgehan Altıntaş <baltintas1711@gmail.com>
2022-02-12 12:45:19 +0300

    Player Movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    //Default movement speed
+    public float speed = 10f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 move = transform.forward;
+
+        //If the W key is pressed player moves forward
+        if (Input.GetKey(KeyCode.W))
+        {
+            controller.Move(move * Time.deltaTime * speed);
+        }
+
+        
+
+        
+    }
+}
