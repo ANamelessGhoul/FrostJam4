a348c507126c0596753b41be770d76460322f6bf
Tecelli Eriz Akintug <tecelli.e.a@gmail.com>
2022-02-12 11:49:59 +0300

    Added basic camera movement for testing

---
+++ Assets/Scripts/Testing/CameraMovement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] private float mouseSensitivity = 60;
+    [SerializeField] private Vector3 forwardVector;
+    private Transform _cameraTransform;
+    private void Awake()
+    {
+        _cameraTransform = transform.GetChild(0);
+    }
+
+    private void Update()
+    {
+        var mouseX = Input.GetAxis("Mouse X");
+        var mouseY = Input.GetAxis("Mouse Y");
+
+        transform.Rotate(new Vector3(0, mouseX * Time.deltaTime * mouseSensitivity, 0) );
+        _cameraTransform.Rotate(new Vector3(-mouseY * Time.deltaTime * mouseSensitivity, 0, 0));
+
+        var forward = Input.GetKey(KeyCode.W) ? 1 : 0;
+
+        forwardVector = transform.InverseTransformDirection(_cameraTransform.forward);
+
+        transform.Translate(forwardVector * (forward * Time.deltaTime ));
+    }
+}
