ab51d683c744be7662ac61861dbfbe13fa2f29a9
Tecelli Eriz Akintug <tecelli.e.a@gmail.com>
2022-02-13 12:18:14 +0300

    Added lens distortion and changed effect scale

--- Assets/Scripts/PostProcessingParameters.cs
+++ Assets/Scripts/PostProcessingParameters.cs
@@ -14,20 +14,24 @@ public class PostProcessingParameters : MonoBehaviour
 
     private FilmGrain _filmGrain;
     private Vignette _vignette;
+    private LensDistortion _lensDistortion;
     private void Start()
     {
         globalVolume.TryGet<FilmGrain>(out _filmGrain);
         globalVolume.TryGet<Vignette>(out _vignette);
+        globalVolume.TryGet<LensDistortion>(out _lensDistortion);
 
         _filmGrain.intensity.Override(0.1f);
         _vignette.intensity.Override(0.2f);
+        _lensDistortion.intensity.Override(0f);
     }
 
     private void Update()
     {
         var distance = (playerTransform.position - demonTransform.position).magnitude;
-        var scale = Mathf.Min(20 / (15 + 5 * distance), 1);
+        var scale = Mathf.Min(30 / (10 + 15 * distance), 1);
         _filmGrain.intensity.Override(scale);
         _vignette.intensity.Override(scale * 0.2f + 0.2f);
+        _lensDistortion.intensity.Override(scale * 0.5f);
     }
 }
